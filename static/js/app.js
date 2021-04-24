87281919a394fc3375346e02d5b457d895376aaa
Emerson Molett <emerson.molett@gmail.com>
2021-04-24 09:47:31 -0500

    populated horizontal bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,38 @@
 // FUNCTION #1 of 4
 function buildCharts(patientID) {
 
-    
-
     d3.json("samples.json").then(data => {
-        console.log(data)
+        console.log(data);
+        // holder for variables in plots
+        var samples = data.samples;
+        var resultArray = samples.filter(s => s.id == patientID);
+        var result = resultArray[0];
+        // console.log(result);
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+        // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        var plot1 = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: otu_ids.slice(0,10).map(x => `OTU ${x}`).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
+            type: "bar",
+            orientation: "h"
+        };
+
+        var data1 = [plot1];
+        var layout1 = {
+            title: "Top 10 OTU Values",
+            xaxis: {
+                title: "# of Values in Sample"
+            },
+            yaxis: {
+                categoryorder: "total ascending"
+            }
+        };
+
+        Plotly.newPlot("bar", data1, layout1);
     })
 };
 
@@ -14,7 +42,7 @@ function populateDemoInfo(patientID) {
     var demographicInfoBox = d3.select("#sample-metadata");
 
     d3.json("samples.json").then(data => {
-        console.log(data)
+        // console.log(data)
     })
 }
 
@@ -40,11 +68,3 @@ function initDashboard() {
 
 initDashboard();
 
-// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-var plot1 = {
-    x: sample_values.slice(0,10),
-    y: otu_ids.map(x => "OTU ${x}"), 
-    text:otu_labels.slice(0,10),
-    type: "bar"
-    orientation: "h"
-};
