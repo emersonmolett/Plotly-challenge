487183ddb423efe931032e08ef32d0e3035e2a74
Emerson Molett <emerson.molett@gmail.com>
2021-04-24 08:03:52 -0500

    started code for horizontal bar chart

--- static/js/app.js
+++ static/js/app.js
@@ -39,3 +39,12 @@ function initDashboard() {
 };
 
 initDashboard();
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+var plot1 = {
+    x: sample_values.slice(0,10),
+    y: otu_ids.map(x => "OTU ${x}"), 
+    text:otu_labels.slice(0,10),
+    type: "bar"
+    orientation: "h"
+};
