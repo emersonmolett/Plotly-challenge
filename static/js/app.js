19f6b23f230516e6f709125b93b4a3846633dd94
Emerson Molett <emerson.molett@gmail.com>
2021-04-24 19:01:01 -0500

    continuing to work on metadata card demonstration

--- static/js/app.js
+++ static/js/app.js
@@ -82,29 +82,27 @@ function populateDemoInfo(patientID) {
 };
 
 //Display the sample metadata card
-var card = d3.select(".card-title")
-Object.entries(data.metadata[0]).forEach(([key, value]) =>
-card.append("p").text(`${key} : ${value}`)
-);
+d3.json("samples.json").then(data => {
+ 
+       
+    // FUNCTION #3 of 4
+    function optionChanged(patientID) {
+        console.log(patientID);
+        buildCharts(patientID);
+        populateDemoInfo(patientID);
+    };
 
-// FUNCTION #3 of 4
-function optionChanged(patientID) {
-    console.log(patientID);
-    buildCharts(patientID);
-    populateDemoInfo(patientID);
-};
+    // FUNCTION #4 of 4
+    function initDashboard() {
+        var dropdown = d3.select("#selDataset")
+        d3.json("samples.json").then((data) => {
+            var patientIDs = data.names;
+            patientIDs.forEach((patientID) => {
+                dropdown.append("option").text(patientID).property("value", patientID)
+            })
+            buildCharts(patientIDs[0]);
+            populateDemoInfo(patientIDs[0]);
+        });
+    };
 
-// FUNCTION #4 of 4
-function initDashboard() {
-    var dropdown = d3.select("#selDataset")
-    d3.json("samples.json").then((data) => {
-        var patientIDs = data.names;
-        patientIDs.forEach((patientID) => {
-            dropdown.append("option").text(patientID).property("value", patientID)
-        })
-        buildCharts(patientIDs[0]);
-        populateDemoInfo(patientIDs[0]);
-    });
-};
-
-initDashboard();
+    initDashboard();
