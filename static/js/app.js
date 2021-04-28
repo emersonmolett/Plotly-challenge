df3bc589b8b2708bf3c24e666ed754424f441f5b
Emerson Molett <emerson.molett@gmail.com>
2021-04-27 19:14:58 -0500

    update prior to submit

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,5 @@
 // FUNCTION #1 of 4
 function buildCharts(patientID) {
-
     d3.json("samples.json").then((data) => {
         // console.log(data);
         // holder for variables in plots
@@ -11,7 +10,6 @@ function buildCharts(patientID) {
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
-
         // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
         var plot1 = {
             x: sample_values.slice(0, 10).reverse(),
@@ -20,7 +18,6 @@ function buildCharts(patientID) {
             type: "bar",
             orientation: "h"
         };
-
         var data1 = [plot1];
         var layout1 = {
             title: "Top 10 OTU Values",
@@ -31,13 +28,9 @@ function buildCharts(patientID) {
                 categoryorder: "total ascending"
             }
         };
-
         Plotly.newPlot("bar", data1, layout1);
     })
-
-
     // Create a bubble chart that displays each sample
-
     d3.json("samples.json").then((data) => {
         // console.log(data);
         // holder for variables in plots
@@ -48,7 +41,6 @@ function buildCharts(patientID) {
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
-
         var plot2 = {
             x: otu_ids.slice(0, 30),
             y: sample_values.slice(0, 30),
@@ -59,35 +51,27 @@ function buildCharts(patientID) {
             },
             text: otu_labels.slice(0, 30)
         };
-
         var data2 = [plot2];
         var layout2 = {
             title: "OTUs in Subjects' Navel",
             showlegend: false,
         };
-
         Plotly.newPlot("bubble", data2, plot2);
-
-
     });
 };
 
 // FUNCTION #2 of 4
+
 //Display the sample metadata card
-
 function populateDemoInfo(patientID) {
-
     var demographicInfoBox = d3.select("#sample-metadata");
-
     d3.json("samples.json").then(data => {
         // console.log(data)
     })
-
     d3.json("samples.json").then((data) => {
         var metadata = data.metadata;
         var resultArray = metadata.filter(s => s.id == patientID);
         var result = resultArray[0];
-
         var demographicInfoBox = d3.select("#sample-metadata");
         demographicInfoBox.html("");
         Object.entries(result).forEach(([key, value]) => {
@@ -95,18 +79,12 @@ function populateDemoInfo(patientID) {
         });
     });
 }
-
-
-
 // FUNCTION #3 of 4
 function optionChanged(patientID) {
     console.log(patientID);
     buildCharts(patientID);
     populateDemoInfo(patientID);
 }
-
-
-
 // FUNCTION #4 of 4
 function initDashboard() {
     var dropdown = d3.select("#selDataset")
@@ -119,7 +97,4 @@ function initDashboard() {
         populateDemoInfo(patientIDs[0]);
     });
 }
-
-
-
 initDashboard();
